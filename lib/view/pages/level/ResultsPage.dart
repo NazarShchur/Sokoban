0f314cf63a69068252b50d1e7ba68f66089ee9a9
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2019-12-26 16:08:12 +0200

    result page instead of dialog

--- lib/view/pages/level/ResultsPage.dart
+++ lib/view/pages/level/ResultsPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:game/controller/service/RecordsService.dart';
 import 'package:game/controller/service/UserService.dart';
 import 'package:game/model/entity/results.dart';
+import 'package:game/view/data/Constants.dart';
 import 'package:game/view/state/NavigationModel.dart';
 import 'package:provider/provider.dart';
 
@@ -13,35 +14,47 @@ class ResultsPage extends StatelessWidget {
     final model = Provider.of<NavigationModel>(context);
     BigInt currentUserId = UserService().getCurrentUserId();
     return Scaffold(
+      backgroundColor: Colors.brown,
       body: FutureBuilder<Results>(
-        future: RecordsService()
-            .getRecordsForUserLevel(currentUserId, results.level),
+        future: RecordsService().getRecordsForUserLevel(currentUserId, results.level),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return Column(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text("your moves: ${results.moves}"),
-                Text(
-                    "your best moves: ${snapshot.data.level != -1 ? snapshot.data.moves : results.moves}"),
-                FlatButton(
-                  onPressed: () {
-                    if (model.nextLevel()) {
-                      Navigator.pop(context);
-                      Navigator.popAndPushNamed(context, "/level");
-                    } else {
-                      Navigator.popAndPushNamed(context, "/levels");
-                    }
-                  },
-                  child: Text("next"),
+                Column(
+                  children: <Widget>[
+                    Text("You Won!", style: style(50)),
+                    Text("Level  ${results.level}", style: style(14),),
+                  ],
                 ),
-                FlatButton(
-                  onPressed: () {
-                    Navigator.popUntil(context, ModalRoute.withName("/levels"));
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, "/levels");
-                  },
-                  child: Text("menu"),
+                snapshot.data.level == -1 ? Text("Reward ${Constants.ADD_MONEY_FOR_LEVEL}",style: style(20)) : Container() ,
+                Text("Your moves: ${results.moves}", style: style(20)),
+                Text("Your best moves: ${snapshot.data.level != -1 ? snapshot.data.moves : results.moves}", style: style(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Constants.buttonBorderColor, width: 10),
+                          color: Constants.buttonBackColor
+                      ),
+                      child: FlatButton(
+                        onPressed: (){_onPressedMenu(context);},
+                        child: Text("menu", style: style(20)),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Constants.buttonBorderColor, width: 10),
+                          color: Constants.buttonBackColor
+                      ),
+                      child: FlatButton(
+                        onPressed: (){_onPressedNext(context, model);},
+                        child: Text("next", style: style(20)),
+                      ),
+                    ),
+                  ],
                 )
               ],
             );
@@ -50,30 +63,23 @@ class ResultsPage extends StatelessWidget {
           }
         },
       ),
-//      actions: [
-//        FlatButton(
-//          onPressed: () {
-//            if(model.nextLevel()){
-//              Navigator.pop(context);
-//              Navigator.popAndPushNamed(context, "/level");
-//            } else {
-//              Navigator.popAndPushNamed(context, "/levels");
-//            }
-//
-//          },
-//          child: Text("next"),
-//
-//        ),
-//        FlatButton(
-//          onPressed: () {
-//            Navigator.popUntil(context, ModalRoute.withName("/levels"));
-//            Navigator.pop(context);
-//            Navigator.pushNamed(context, "/levels");
-//          },
-//          child: Text("menu"),
-//
-//        )
-//      ],
     );
   }
+}
+
+void _onPressedNext(context, model) {
+  if (model.nextLevel()) {
+    Navigator.popAndPushNamed(context, "/level");
+  } else {
+    Navigator.popAndPushNamed(context, "/levels");
+  }
+}
+
+void _onPressedMenu(context) {
+  Navigator.popUntil(context, ModalRoute.withName("/levels"));
+  Navigator.pop(context);
+  Navigator.pushNamed(context, "/levels");
+}
+TextStyle style(double fontSize){
+  return TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: fontSize);
 }
